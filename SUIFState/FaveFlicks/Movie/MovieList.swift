b026b2b2e917d6d2edb36f86e872d43654e675c5
ian8829 <ghjkghj@naver.com>
2021-07-19 21:22:54 +0900

    use ObservableObject

--- SUIFState/FaveFlicks/Movie/MovieList.swift
+++ SUIFState/FaveFlicks/Movie/MovieList.swift
@@ -33,39 +33,41 @@
 import SwiftUI
 
 struct MovieList: View {
-  @StateObject var movieStore = MovieStore()
-  @State private var isPresented = false
+	@StateObject var movieStore = MovieStore()
+	@State private var isPresented = false
+	@ObservedObject var userStore = UserStore()
 
-  var body: some View {
-    NavigationView {
-      List {
-        ForEach(movieStore.movies, id: \.title) {
-          MovieRow(movie: $0)
-        }
-        .onDelete(perform: movieStore.deleteMovie)
-      }
-      .sheet(isPresented: $isPresented) {
-        AddMovie(movieStore: movieStore, showModal: $isPresented)
-      }
-      .navigationBarTitle(Text("Fave Flicks"))
-      .navigationBarItems(
-        leading:
-          NavigationLink(destination: UserView()) {
-            HStack {
-              Image(systemName: "person.fill")
-            }
-          },
-        trailing:
-          Button(action: { isPresented.toggle() }) {
-            Image(systemName: "plus")
-          }
-      )
-    }
-  }
+	var body: some View {
+		NavigationView {
+			List {
+				ForEach(movieStore.movies, id: \.title) {
+					MovieRow(movie: $0)
+				}
+			}
+			.sheet(isPresented: $isPresented) {
+				AddMovie(movieStore: movieStore, showModal: $isPresented)
+			}
+			.navigationBarTitle(Text("Fave Flicks"))
+			.navigationBarItems(
+				leading:
+					NavigationLink(
+						destination: UserView(userStore: userStore)) {
+						HStack {
+							Text(userStore.currentUserInfo.userName)
+							Image(systemName: "person.fill")
+						}
+					},
+				trailing:
+					Button(action: { isPresented.toggle() }) {
+						Image(systemName: "plus")
+					}
+			)
+		}
+	}
 }
 
 struct MovieList_Previews: PreviewProvider {
-  static var previews: some View {
-    MovieList(movieStore: MovieStore())
-  }
+	static var previews: some View {
+		MovieList(movieStore: MovieStore())
+	}
 }
--- SUIFState/FaveFlicks/User/UserStore.swift
+++ SUIFState/FaveFlicks/User/UserStore.swift
@@ -29,6 +29,12 @@
 /// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
+import Combine
+
+class UserStore: ObservableObject {
+	@Published var currentUserInfo = UserInfo(userName: "Movie Lover", favoriteGenre: .action)
+}
+
 
 struct UserInfo {
   let userName: String
