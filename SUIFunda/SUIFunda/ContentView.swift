dfef830c6ff268a77e1767e7d6acf300571e0bc8
ian8829 <ghjkghj@naver.com>
2021-07-19 17:12:07 +0900

    embeded with HStack

--- SUIFunda/SUIFunda/ContentView.swift
+++ SUIFunda/SUIFunda/ContentView.swift
@@ -9,47 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 20.0) {
-            VStack(alignment: .leading) {
-                Text("Meow! Mix")
-                    .font(.title)
-                    .fontWeight(.semibold)
-                
-                Text("Apple Music for Ozma")
-                    .font(.title)
-                    .fontWeight(.light)
-                    .foregroundColor(.accentColor)
-                
-                Text("Updated Caturday")
-                    .font(Font.system(.body).smallCaps())
-            }
+        HStack(alignment: .bottom, spacing: 20.0) {
+            Thumbnail()
             
-            Text("Whether you're a kitten or an old-timer, get meowing with this purrsonalized mix of music to meow to!")
-            
-            HStack(spacing: 12.0) {
-                Button { print("Play!") }
-                    label: {
-                        ButtonLabel(title: "Play", systemImage: "play.fill")
-                    }
-                
-                Button { print("Shuffle!") }
-                    label: {
-                        ButtonLabel(title: "Shuffle", systemImage: "shuffle")
-                    }
+            VStack {
+                MixDescription()
+                Buttons()
             }
         }
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        Image("meowMix")
-            .resizable()
-            .frame(maxHeight: 250)
-            .previewLayout(.sizeThatFits)
-            
-        
-        ContentView()
+        .padding()
+        .frame(minHeight: 150, maxHeight: 250)
     }
 }
 
@@ -68,3 +37,91 @@ struct ButtonLabel: View {
         .cornerRadius(12)
     }
 }
+
+struct Thumbnail: View {
+    var body: some View {
+        ZStack(alignment: Alignment(horizontal: .leading, vertical: .top), content: {
+            ZStack {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.accentColor)
+                    .aspectRatio(1, contentMode: .fit)
+                    .shadow(radius: 10)
+                
+                Image(systemName: "play.fill")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .foregroundColor(.white)
+                    .opacity(0.4)
+            }
+            
+            VStack(alignment: .leading, content: {
+                Text("Meow!")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                
+                Text("Mix")
+                    .font(.largeTitle)
+                
+                Spacer()
+            })
+            .foregroundColor(.white)
+            .padding()
+        })
+    }
+}
+
+struct Buttons: View {
+    var body: some View {
+        HStack(spacing: 12.0) {
+            Button { print("Play!") }
+                label: {
+                    ButtonLabel(title: "Play", systemImage: "play.fill")
+                }
+            
+            Button { print("Shuffle!") }
+                label: {
+                    ButtonLabel(title: "Shuffle", systemImage: "shuffle")
+                }
+        }
+    }
+}
+
+struct MixDescription: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20.0) {
+            VStack(alignment: .leading) {
+                Text("Meow! Mix")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                
+                Text("Apple Music for Ozma")
+                    .font(.title)
+                    .fontWeight(.light)
+                    .foregroundColor(.accentColor)
+                
+                Text("Updated Caturday")
+                    .font(Font.system(.body).smallCaps())
+            }
+            
+            Text("Whether you're a kitten or an old-timer, get meowing with this purrsonalized mix of music to meow to!")
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Image("meowMix")
+            .resizable()
+            .frame(maxHeight: 250)
+            .previewLayout(.sizeThatFits)
+            
+        
+        ContentView()
+        
+        Thumbnail()
+            .padding()
+            .previewLayout(.fixed(width: 250, height: 250))
+    }
+}
+
