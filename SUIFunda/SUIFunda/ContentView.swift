e0cb91d0c16e4ab3171364d81e32c563767571c2
ian8829 <ghjkghj@naver.com>
2021-07-19 16:27:03 +0900

    state and binding 2

--- SUIFunda/SUIFunda/ContentView.swift
+++ SUIFunda/SUIFunda/ContentView.swift
@@ -8,15 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var swiftyColor: Color = .red
-    @State var swiftyOpacity: Double = 0.7
+    @State private var swiftyColor: Color = .red
+    @State private var swiftyOpacity: Double = 0.7
     
     var body: some View {
         VStack {
-            ColorPicker("Swifty Color", selection: $swiftyColor)
-            
-            Slider(value: $swiftyOpacity, in: 0...1)
-                .accentColor(swiftyColor)
+            SwiftyControls(
+                swiftyColor: $swiftyColor,
+                swiftyOpacity: $swiftyOpacity
+            )
             
             Image(systemName: "swift")
                 .resizable()
@@ -30,6 +30,21 @@ struct ContentView: View {
         .padding(20)
     }
 }
+
+struct SwiftyControls: View {
+    @Binding var swiftyColor: Color
+    @Binding var swiftyOpacity: Double
+
+    var body: some View {
+        VStack {
+            ColorPicker("Swifty Color", selection: $swiftyColor)
+            
+            Slider(value: $swiftyOpacity, in: 0...1)
+                .accentColor(swiftyColor)
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
