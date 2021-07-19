ff287f3d184a1fab1eca417655504c538401daa6
ian8829 <ghjkghj@naver.com>
2021-07-19 14:51:04 +0900

    init 2

---
+++ SUIFunda/SUIFunda/SUIFundaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIFundaApp.swift
+//  SUIFunda
+//
+//  Created by ian white on 2021/07/19.
+//
+
+import SwiftUI
+
+@main
+struct SUIFundaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
